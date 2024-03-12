917e842c356d8bb1e9609118341cb0132602f7e9
Voldi <vmonz093@uottawa.com>
2024-03-12 14:31:44 -0400

    initial code done -> debuging : image not found

--- readImage.go
+++ readImage.go
@@ -1,65 +1,63 @@
 package main
 
 import (
-	"image"
 	_ "image/jpeg"
-	"os"
 )
 
-type Histo struct {
-	Name string
-	H    []int
-}
+// type Histo struct {
+// 	Name string
+// 	H    []float32
+// }
 
-// adapted from: first example at pkg.go.dev/image
-func computeHistogram(imagePath string, depth int) (Histo, error) {
-	// Open the JPEG file
-	file, err := os.Open(imagePath)
-	if err != nil {
-		return Histo{"", nil}, err
-	}
-	defer file.Close()
+// // adapted from: first example at pkg.go.dev/image
+// func computeHistogram(imagePath string, depth int) (Histo, error) {
+// 	// Open the JPEG file
+// 	file, err := os.Open(imagePath)
+// 	if err != nil {
+// 		return Histo{"", nil}, err
+// 	}
+// 	defer file.Close()
 
-	// Decode the JPEG image
-	img, _, err := image.Decode(file)
-	if err != nil {
-		return Histo{"", nil}, err
-	}
+// 	// Decode the JPEG image
+// 	img, _, err := image.Decode(file)
+// 	if err != nil {
+// 		return Histo{"", nil}, err
+// 	}
 
-	// Get the dimensions of the image
-	bounds := img.Bounds()
-	width, height := bounds.Max.X, bounds.Max.Y
-	result := make([]int, depth)
-	rgbMax := make([]int, 10)
-	// Scaning the RGB values for the image
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
+// 	// Get the dimensions of the image
+// 	bounds := img.Bounds()
+// 	width, height := bounds.Max.X, bounds.Max.Y
+// 	result := make([]float32, depth)
+// 	rgbMax := make([]float32, 10)
+// 	// Scaning the RGB values for the image
+// 	for y := 0; y < height; y++ {
+// 		for x := 0; x < width; x++ {
 
-			// Convert the pixel to RGBA
-			red, green, blue, _ := img.At(x, y).RGBA()
-			// A color's RGBA method returns values in the range [0, 65535].
-			// Shifting by 8 reduces this to the range [0, 255].
-			red >>= 8
-			blue >>= 8
-			green >>= 8
+// 			// Convert the pixel to RGBA
+// 			red, green, blue, _ := img.At(x, y).RGBA()
+// 			// A color's RGBA method returns values in the range [0, 65535].
+// 			// Shifting by 8 reduces this to the range [0, 255].
+// 			red >>= 8
+// 			blue >>= 8
+// 			green >>= 8
 
-			//Data for normalization
-			rgbMax[0] += int(red)
-			rgbMax[1] += int(blue)
-			rgbMax[2] += int(green)
+// 			//Data for normalization
+// 			rgbMax[0] += float32(red)
+// 			rgbMax[1] += float32(blue)
+// 			rgbMax[2] += float32(green)
 
-			result = append(result, int(red), int(blue), int(green))
-		}
-	}
+// 			result = append(result, float32(red), float32(blue), float32(green))
+// 		}
+// 	}
 
-	//Normalizing the Histogram
-	for i, value := range result {
-		result[i] = value / rgbMax[i%3]
-	}
+// 	//Normalizing the Histogram
+// 	for i, value := range result {
+// 		result[i] = value / rgbMax[i%3]
+// 	}
 
-	h := Histo{imagePath, result}
-	return h, nil
-}
+// 	h := Histo{imagePath, result}
+// 	return h, nil
+// }
 
 // func main() {
 // 	// read the image name from command line
--- similaritySearch.go
+++ similaritySearch.go
@@ -2,12 +2,70 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 	"sync"
 )
+
+type Histo struct {
+	Name string
+	H    []float32
+}
+
+// adapted from: first example at pkg.go.dev/image
+func computeHistogram(imagePath string, depth int) (Histo, error) {
+	// Open the JPEG file
+	file, err := os.Open(imagePath)
+	if err != nil {
+		return Histo{"", nil}, err
+	}
+	defer file.Close()
+
+	// Decode the JPEG image
+	file.Seek(0, 0)
+	img, _, err := image.Decode(file)
+	fmt.Print("|| USED ||")
+	if err != nil {
+		return Histo{"", nil}, err
+	}
+	fmt.Print("|| USED ||")
+	// Get the dimensions of the image
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+	result := make([]float32, depth)
+	rgbMax := make([]float32, 10)
+	// Scaning the RGB values for the image
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+
+			// Convert the pixel to RGBA
+			red, green, blue, _ := img.At(x, y).RGBA()
+			// A color's RGBA method returns values in the range [0, 65535].
+			// Shifting by 8 reduces this to the range [0, 255].
+			red >>= 8
+			blue >>= 8
+			green >>= 8
+
+			//Data for normalization
+			rgbMax[0] += float32(red)
+			rgbMax[1] += float32(blue)
+			rgbMax[2] += float32(green)
+
+			result = append(result, float32(red), float32(blue), float32(green))
+		}
+	}
+
+	//Normalizing the Histogram
+	for i, value := range result {
+		result[i] = value / rgbMax[i%3]
+	}
+
+	h := Histo{imagePath, result}
+	return h, nil
+}
 
 func readFiles(directoryPath string) (filenames []string) {
 	files, err := ioutil.ReadDir(directoryPath)
@@ -52,8 +110,8 @@ func splitSlice(slice []string, k int) [][]string {
 	return result
 }
 
-func computeHistograms(imagePaths []string, depth int, hChan chan<- Histo) {
-	defer close(hChan)
+func computeHistograms(imagePaths []string, depth int, wg *sync.WaitGroup, hChan chan<- Histo) {
+	defer wg.Done()
 
 	for _, imagePath := range imagePaths {
 		val, err := computeHistogram(imagePath, depth)
@@ -64,25 +122,76 @@ func computeHistograms(imagePaths []string, depth int, hChan chan<- Histo) {
 	}
 }
 
+func min(a, b float32) float32 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func compareHistograms(h1 Histo, h2 Histo) (result float32) {
+	result = 0.0
+	for i, _ := range h2.H {
+		result += min(h1.H[i], h2.H[i])
+	}
+	return result
+}
+
+type Pair struct {
+	h               Histo
+	distanceToQuery float32
+}
+
+func minPair(slice []Pair) (index int) {
+	index = 0
+	for i, value := range slice {
+		if value.distanceToQuery < slice[index].distanceToQuery {
+			index = i
+		}
+	}
+	return index
+}
+
 func main() {
-	args := os.Args
+	fmt.Print("|| STARTING ||")
+	args := []string{"/Users/voldischool/Documents/GO-Projects/Similarity Image Search/res/queryImages/q00.jpg",
+		"/Users/voldischool/Documents/GO-Projects/Similarity Image Search/res/imageDataset2_15_20/"}
 
 	wg := sync.WaitGroup{}
 
-	histogramChannel := make(chan Histo)
 	k := 1
 	dataset := splitSlice(readFiles(args[1]), k)
+	histogramChannel := make(chan Histo, len(dataset[0])*len(dataset))
 
 	for _, subSlice := range dataset {
 		wg.Add(1)
-		go computeHistograms(subSlice, 10, histogramChannel)
+		go computeHistograms(subSlice, 10, &wg, histogramChannel)
 	}
+	close(histogramChannel)
 
 	queryImage, err := computeHistogram(args[0], 10)
-
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 		return
+	}
+	wg.Wait()
+
+	highest := make([]Pair, 0, 5)
+	for value := range histogramChannel {
+		pair := Pair{value, compareHistograms(queryImage, value)}
+		if len(highest) != 5 {
+			highest = append(highest, pair)
+			continue
+		}
+
+		smallerIndex := minPair(highest)
+		if pair.distanceToQuery > highest[smallerIndex].distanceToQuery {
+			highest[smallerIndex] = pair
+		}
+	}
+
+	for _, value := range highest {
+		fmt.Print("|| " + value.h.Name + " ||")
 	}
 
 }
